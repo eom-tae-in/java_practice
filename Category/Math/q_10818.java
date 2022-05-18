4cc9ab0d6539a51b37a8622c13c3aef88b934a88
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-05-18 22:06:05 +0900

    Add files via upload

---
+++ Category/Math/q_10818.java
@@ -0,0 +1,23 @@
+package Category.Math;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+public class q_10818 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N - 1]);
+    }
+}
