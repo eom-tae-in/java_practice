ea3c574cb92e82e1e4444da8fb98ffc2d27d68ca
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-05-21 19:31:08 +0900

    Add files via upload

---
+++ Category/Math/q_2839.java
@@ -0,0 +1,26 @@
+package Category.Math;
+
+import java.util.Scanner;
+
+
+public class q_2839 {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+
+        if (a == 4 || a == 7) {
+            System.out.println(-1);
+        } else if (a % 5 == 0) {
+            System.out.println(a / 5);
+        } else if (a % 5 == 1 || a % 5 == 3) {
+            System.out.println((a / 5) + 1);
+        } else if (a % 5 == 2 || a % 5 == 4) {
+            System.out.println((a / 5) + 2);
+        }
+    }
+}
