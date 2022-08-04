ef74856d553777c09ef6ab99323607df0e330aef
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-08-04 15:39:35 +0900

    Add files via upload

---
+++ Category/q_11134.java
@@ -0,0 +1,32 @@
+package Category;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_11134 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++){
+
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            if (N % C == 0){
+
+                sb.append(N / C).append("\n");
+            }
+            else {
+                sb.append(N / C + 1).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
