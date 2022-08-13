e040784e0a474896cea8394574788268affdb274
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-08-13 20:10:57 +0900

    Add files via upload

---
+++ Category/q_2845.java
@@ -0,0 +1,36 @@
+package Category;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_2845 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int L = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[5];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < 5; i++) {
+
+            sb.append(arr[i] - L * P).append(" ");
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
