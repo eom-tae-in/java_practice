5b57e72ff934e24c5aa3fafbcc75e93718968033
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-08-14 21:21:36 +0900

    Add files via upload

---
+++ Category/q_10886.java
@@ -0,0 +1,28 @@
+package Category;
+
+import java.util.Scanner;
+
+public class q_10886 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        int C = 0;
+        int NC = 0;
+        for (int i = 0; i < T; i++) {
+            if (sc.nextInt() == 1) {
+                C++;
+            } else {
+                NC++;
+            }
+        }
+        if (C > NC) {
+            System.out.println("Junhee is cute!");
+        } else {
+            System.out.println("Junhee is not cute!");
+        }
+    }
+}
