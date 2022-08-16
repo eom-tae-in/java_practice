c8f94f793b7277590e6094c6253944f23a788613
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-08-16 19:40:34 +0900

    Add files via upload

---
+++ Category/q_2010.java
@@ -0,0 +1,25 @@
+package Category;
+
+import java.util.Scanner;
+
+public class q_2010 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < T; i++) {
+
+            int A = sc.nextInt();
+            sum += A;
+            count = i;
+
+        }
+        System.out.println(sum - count);
+
+    }
+}
