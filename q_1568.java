9aaf09bf97f11922117aef0bd7e000db77d214a4
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-09-01 22:55:00 +0900

    Add files via upload

---
+++ q_1568.java
@@ -0,0 +1,27 @@
+package Category;
+
+import java.util.Scanner;
+
+public class q_1568 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int count = 0;
+        int num = 1;
+
+        while (T >= num) {
+
+            T -= num;
+            num++;
+            count++;
+            if (T < num) {
+                num = 1;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
