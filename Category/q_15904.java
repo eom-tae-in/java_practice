1c4b87076f6f434bc783c414d23bf4df9390374f
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-09-06 22:47:06 +0900

    Add files via upload

---
+++ Category/q_15904.java
@@ -0,0 +1,30 @@
+package Category;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_15904 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        char[] ucpc = {'U', 'C', 'P', 'C'};
+        int index = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (ucpc[index] == str.charAt(i)) {
+                index++;
+            }
+
+            if (index == 4) {
+                break;
+            }
+        }
+
+        System.out.println(index == 4 ? "I love UCPC" : "I hate UCPC");
+    }
+}
