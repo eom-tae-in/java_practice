371cf797b64a68fb7a50405acf857b229a4c59ed
eom-tae-in <eti0728@daum.net>
2022-09-13 16:21:51 +0900

    Fix:q_5585(Scanner -> BufferedReader)

--- Category/baekjoon/q_5585.java
+++ Category/baekjoon/q_5585.java
@@ -1,15 +1,17 @@
 package Category.baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class q_5585 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] moneytype = {500, 100, 50, 10, 5, 1};
 
-        int money = 1000 - sc.nextInt();
+        int money = 1000 -Integer.parseInt(br.readLine());
         int count = 0;
 
         for (int i = 0; i < moneytype.length; i++) {
