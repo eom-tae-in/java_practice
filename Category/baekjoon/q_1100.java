70e2ce440f0fb700c3cca18c059bd5e12b306e49
eom-tae-in <eti0728@daum.net>
2022-10-11 02:37:57 +0900

    Feat: q_1100

---
+++ Category/baekjoon/q_1100.java
@@ -0,0 +1,34 @@
+package Category.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_1100 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) {
+
+            String str = br.readLine();
+            String s = "";
+            if (i % 2 == 0) {
+                for (int j = 0; j < 8; j += 2) {
+                    if (str.charAt(j) == 'F') {
+                        count++;
+                    }
+                }
+            } else {
+                for (int j = 1; j < 8; j += 2) {
+                    if (str.charAt(j) == 'F') {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
