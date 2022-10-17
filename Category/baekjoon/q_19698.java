73a87de49ae0c6b082bcd0ba22077249c59e7297
eom-tae-in <eti0728@daum.net>
2022-10-18 00:41:00 +0900

    Feat: q_19698

---
+++ Category/baekjoon/q_19698.java
@@ -0,0 +1,28 @@
+package Category.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_19698 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        W /= L;
+        H /= L;
+
+        System.out.println(Math.min(W * H, N));
+
+
+
+    }
+}
