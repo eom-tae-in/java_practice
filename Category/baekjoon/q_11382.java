e119b14fd9d60f67634d7043c93636c6556922e4
eom-tae-in <eti0728@daum.net>
2022-10-29 23:57:57 +0900

    Feat: q_11382

---
+++ Category/baekjoon/q_11382.java
@@ -0,0 +1,24 @@
+package Category.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class q_11382 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A + B + C);
+    }
+
+
+
+}
