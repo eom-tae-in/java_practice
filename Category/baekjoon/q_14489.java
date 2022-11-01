069b885ce42993a9d7e6bf7d48a81d98c61fc058
eom-tae-in <eti0728@daum.net>
2022-11-01 23:09:19 +0900

    Feat: q_14489

---
+++ Category/baekjoon/q_14489.java
@@ -0,0 +1,25 @@
+package Category.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_14489 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+
+        if (A + B >= 2 * C) {
+            System.out.println((A + B) - 2 * C);
+            return;
+        }
+        System.out.println(A + B);
+    }
+}
