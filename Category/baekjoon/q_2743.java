95564ce3518b7b56c5ebc06a568958981b24f93a
eom-tae-in <eti0728@daum.net>
2022-11-04 23:20:20 +0900

    Feat: q_2743

---
+++ Category/baekjoon/q_2743.java
@@ -0,0 +1,18 @@
+package Category.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_2743 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str.length());
+
+    }
+}
