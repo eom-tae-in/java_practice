5eb8f24df5e0b15dff82e03721df5258fc11cc95
eom-tae-in <eti0728@daum.net>
2022-11-14 02:25:36 +0900

    Feat: 자릿수 더하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,7 +1,5 @@
 package Category.programmers;
 
-import Category.programmers.Programmers.Solution;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Programmers {
@@ -11,23 +9,16 @@ public class Programmers {
             Scanner sc = new Scanner(System.in);
             int T = sc.nextInt();
             System.out.println(solution(T));
-        }
 
-        public static int solution(int n) {
-            int answer = 0;
-            if (n == 1) {
-                answer = 1;
-                return answer;
-            }
-            for (int i = 1; i <= (int)Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    if (i != n / i) {
-                        answer += n / i;
-                    }
-                    answer += i;
+        }
+            public static int solution(int n) {
+                int answer = 0;
+                String str = Integer.toString(n);
+                for (int i = 0; i < str.length(); i++) {
+                    answer += str.charAt(i) - '0';
                 }
-            }
-            return answer;
+                System.out.println("Hello Java");
+                return answer;
         }
     }
 }
