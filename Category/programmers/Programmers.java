2afbdc7bbf79d820aece46e97769472ae978243c
eom-tae-in <eti0728@daum.net>
2022-11-13 23:51:10 +0900

    Feat: 약수의 합

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,6 @@
 package Category.programmers;
 
+import Category.programmers.Programmers.Solution;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,24 +8,26 @@ public class Programmers {
 
     static class Solution {
         public static void main(String[] args) {
-
             Scanner sc = new Scanner(System.in);
-
             int T = sc.nextInt();
-            int[] arr = new int[T];
-
-            for (int i = 0; i < T; i++) {
-                arr[i] = sc.nextInt();
-            }
-            System.out.println(solution(arr));
+            System.out.println(solution(T));
         }
 
-        public static double solution(int[] arr) {
-            double answer = 0;
-            for (int i = 0; i < arr.length; i++) {
-                answer += arr[i];
+        public static int solution(int n) {
+            int answer = 0;
+            if (n == 1) {
+                answer = 1;
+                return answer;
             }
-            return answer / arr.length;
+            for (int i = 1; i <= (int)Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    if (i != n / i) {
+                        answer += n / i;
+                    }
+                    answer += i;
+                }
+            }
+            return answer;
         }
     }
 }
