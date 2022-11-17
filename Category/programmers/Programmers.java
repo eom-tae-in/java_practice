0f31d4fa8b80907b4e3f98bf6e9e573c95cd4739
eom-tae-in <eti0728@daum.net>
2022-11-17 17:06:29 +0900

    Feat: 문자열을 정수로 바꾸기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -12,20 +12,9 @@ public class Programmers {
             System.out.println(solution(str));
         }
 
-        static boolean solution(String s) {
-            boolean answer = true;
-            int count = 0;
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
-                    count++;
-                }
-                if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
-                    count--;
-                }
-            }
-            if (count != 0) {
-                answer = false;
-            }
+        public static int solution(String s) {
+            int answer = 0;
+            answer = Integer.parseInt(s);
             return answer;
         }
     }
