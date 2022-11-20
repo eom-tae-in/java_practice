c0750ede3cb6be01d2d15ec50ff28b33c7a29b0d
eom-tae-in <eti0728@daum.net>
2022-11-21 02:40:40 +0900

    Feat: 나머지가 1이 되는 수 찾기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,6 +1,9 @@
 package Category.programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programmers {
@@ -8,15 +11,17 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int x = sc.nextInt();
             int n = sc.nextInt();
-            System.out.println(Arrays.toString(solution(x, n)));
+            System.out.println(solution(n));
         }
 
-        public static long[] solution(int x, int n) {
-            long[] answer = new long[n];
-            for (int i = 0; i < n; i++) {
-                answer[i] = (long) x * (i + 1);
+        public static int solution(int n) {
+            int answer = 0;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 1) {
+                    answer = i;
+                    break;
+                }
             }
             return answer;
         }
