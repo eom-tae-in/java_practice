fc2c8a6f7c5cf2eacfc1490019e7a180a96e3a6a
eom-tae-in <eti0728@daum.net>
2022-11-20 20:24:21 +0900

    Feat: x만큼 간격이 있는 n개의 숫자

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,7 +1,6 @@
 package Category.programmers;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Programmers {
@@ -9,17 +8,16 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            long n = sc.nextLong();
-            System.out.println(solution(n));
+            int x = sc.nextInt();
+            int n = sc.nextInt();
+            System.out.println(Arrays.toString(solution(x, n)));
         }
 
-        public static long solution(long n) {
-            long answer = 0;
-            String str = Long.toString(n);
-            String[] strings = str.split("");
-            Arrays.sort(strings, Collections.reverseOrder());
-            String sortedStr = String.join("", strings);
-            answer = Long.parseLong(sortedStr);
+        public static long[] solution(int x, int n) {
+            long[] answer = new long[n];
+            for (int i = 0; i < n; i++) {
+                answer[i] = (long) x * (i + 1);
+            }
             return answer;
         }
     }
