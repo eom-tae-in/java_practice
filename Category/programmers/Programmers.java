555901a4022024b5ca12ff99a489450815e90fbe
eom-tae-in <eti0728@daum.net>
2022-11-23 20:02:38 +0900

    Feat: 콜라츠 추측

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -8,15 +8,30 @@ public class Programmers {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            int m = sc.nextInt();
-            System.out.println(solution(n, m));
+            System.out.println(solution(n));
         }
-            public static long solution(int a, int b) {
-                long answer = 0;
-                for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
-                    answer += i;
+        public static int solution(int num) {
+            int answer = 0;
+            int count = 0;
+            long number = num;
+            for (int i = 0; i < 500; i++) {
+                if (number != 1) {
+                    count++;
+                    if (number % 2 == 0) {
+                        number /= 2;
+                    } else {
+                        number = 3 * number + 1;
+                    }
+                } else {
+                    break;
                 }
-                return answer;
             }
+            if (count == 500 && number != 1) {
+                answer = -1;
+            } else {
+                answer = count;
+            }
+            return answer;
+        }
     }
 }
