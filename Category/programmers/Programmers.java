f5955b82bb9c02568390e6210551b204a49b90f0
eom-tae-in <eti0728@daum.net>
2022-11-25 18:13:28 +0900

    두 수의 차

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,7 +1,4 @@
 package Category.programmers;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -9,32 +6,14 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String strings = sc.next();
-            System.out.println(solution(strings));
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(solution(num1, num2));
         }
-        public int[] solution(int[] arr, int divisor) {
-            int[] answer;
-            int index = 0;
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int j : arr) {
-                if (j % divisor == 0) {
-                    list.add(j);
-                }
-            }
-            if (list.size() == 0) {
-                answer = new int[]{-1};
-            } else {
+    }
 
-                answer = new int[list.size()];
-
-                for (int i = 0; i < answer.length; i++) {
-                    answer[i] = list.get(i);
-                }
-
-                Arrays.sort(answer);
-            }
-
-            return answer;
-        }
+    public static int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
     }
 }
