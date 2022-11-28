23484c096636b265fbe2c87a65f850c11605b436
eom-tae-in <eti0728@daum.net>
2022-11-28 12:13:33 +0900

    Feat: 몫 구하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -13,7 +13,7 @@ public class Programmers {
     }
 
     public static int solution(int num1, int num2) {
-        int answer = num1 % num2;
+        int answer = num1 / num2;
         return answer;
     }
 }
