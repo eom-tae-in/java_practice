48f419bb7e040e6d060290409602ae8befc62787
eom-tae-in <eti0728@daum.net>
2022-11-30 18:29:26 +0900

    Feat: 숫자 비교하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -12,8 +12,13 @@ public class Programmers {
         }
     }
 
-    public static int solution(int num1, int num2) {
-        int answer = num1 * num2;
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (num1 == num2) {
+            answer = 1;
+        } else {
+            answer = -1;
+        }
         return answer;
     }
 }
