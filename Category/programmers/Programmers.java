3e4429b99b0d9209a1d0b5b07e16ddf30697ef81
eom-tae-in <eti0728@daum.net>
2022-12-03 22:11:57 +0900

    Feat: 각도기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -6,14 +6,22 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
-            System.out.println(solution(num1, num2));
+            int angle = sc.nextInt();
+            System.out.println(solution(angle));
         }
     }
 
-    public static int solution(int num1, int num2) {
-        int answer = num1 + num2;
+    public static int solution(int angle) {
+        int answer = 0;
+        if (angle < 90) {
+            answer = 1;
+        } else if (angle == 90) {
+            answer = 2;
+        } else if (angle < 180) {
+            answer = 3;
+        } else {
+            answer = 4;
+        }
         return answer;
     }
 }
