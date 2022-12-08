dd6b5812a451617b1ba84e858c4e78bcf86da3ae
eom-tae-in <eti0728@daum.net>
2022-12-08 11:03:25 +0900

    Feat: 중복된 숫자 개수

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -6,16 +6,21 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-            System.out.println(solution(num));
+            int index = sc.nextInt();
+            int[] array = new int[index];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = sc.nextInt();
+            }
+            int number = sc.nextInt();
+            System.out.println(solution(array, number));
         }
     }
 
-    public static int solution(int n) {
+    public static int solution(int[] array, int n) {
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                answer += i;
+        for (int j : array) {
+            if (j == n) {
+                answer++;
             }
         }
         return answer;
