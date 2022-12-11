2bb91aa1c386b0d77204e45d82d409889499710d
eom-tae-in <eti0728@daum.net>
2022-12-11 12:25:52 +0900

    Feat: 배열 뒤집기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,7 @@
 package Category.programmers;
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Programmers {
@@ -7,17 +9,19 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-            System.out.println(solution(num));
+            int index = sc.nextInt();
+            int[] arrayNumber = new int[index];
+            for (int i = 0; i < arrayNumber.length; i++) {
+                arrayNumber[i] = sc.nextInt();
+            }
+            System.out.println(Arrays.toString(solution(arrayNumber)));
         }
     }
 
-    public static int solution(int n) {
-        int answer = 0;
-        if (n % 7 == 0) {
-            answer = n / 7;
-        } else {
-            answer = n / 7 + 1;
+    public static int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[num_list.length - (i + 1)];
         }
         return answer;
     }
