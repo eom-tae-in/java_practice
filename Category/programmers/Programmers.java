e05fc3e91ea5eafc86b35096523ae9dcfe35f620
eom-tae-in <eti0728@daum.net>
2022-12-14 22:01:13 +0900

    Feat: 배열 자르기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -9,17 +9,24 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String str = sc.next();
-            System.out.println(solution(str));
+            int index = sc.nextInt();
+            int[] numbers = new int[index];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = sc.nextInt();
+            }
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(Arrays.toString(solution(numbers, num1, num2)));
         }
     }
 
-    public static String solution(String my_string) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        sb.append(my_string);
-        sb.reverse();
-        answer = String.valueOf(sb);
+    public static int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        int index = 0;
+        for (int i = num1; i <= num2; i++) {
+            answer[index] = numbers[i];
+            index++;
+        }
         return answer;
     }
 }
