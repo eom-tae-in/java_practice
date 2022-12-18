e2948343466b01098c94899e47b1c1bad548c06a
eom-tae-in <eti0728@daum.net>
2022-12-18 14:55:43 +0900

    Feat: 최댓값 만들기 (1)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -9,17 +9,19 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int money = sc.nextInt();
-            System.out.println(Arrays.toString(solution(money)));
+            int length = sc.nextInt();
+            int[] arr = new int[length];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(solution(arr));
         }
     }
 
-    public static int[] solution(int money) {
-        int[] answer = new int[2];
-        int num = money / 5500;
-        int R = money % 5500;
-        answer[0] = num;
-        answer[1] = R;
+    public static int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
         return answer;
     }
 }
