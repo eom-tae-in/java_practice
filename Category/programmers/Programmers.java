b689bd154d78b3b64db70ad826e3996b1a5c66dd
eom-tae-in <eti0728@daum.net>
2022-12-19 16:20:27 +0900

    Feat: 편지

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,27 +1,20 @@
 package Category.programmers;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int length = sc.nextInt();
-            int[] arr = new int[length];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = sc.nextInt();
-            }
-            System.out.println(solution(arr));
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String message = br.readLine();
+            System.out.println(solution(message));
         }
     }
 
-    public static int solution(int[] numbers) {
-        int answer = 0;
-        Arrays.sort(numbers);
-        answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    public static int solution(String message) {
+        int answer = message.length() * 2;
         return answer;
     }
 }
