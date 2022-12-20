4e0c2137a6afb0aa1cd65d6ac0a2d74a6e3f1244
eom-tae-in <eti0728@daum.net>
2022-12-20 20:32:32 +0900

    Feat: 배열 원소의 길이

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,20 +1,27 @@
 package Category.programmers;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String message = br.readLine();
-            System.out.println(solution(message));
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int index = sc.nextInt();
+            String[] arr = new String[index];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.next();
+            }
+            System.out.println(Arrays.toString(solution(arr)));
         }
     }
 
-    public static int solution(String message) {
-        int answer = message.length() * 2;
+    public static int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = strlist[i].length();
+        }
         return answer;
     }
 }
