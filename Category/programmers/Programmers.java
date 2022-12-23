e40b3a624195a16c7bee12b20a08e85b5bc2f132
eom-tae-in <eti0728@daum.net>
2022-12-23 19:12:35 +0900

    Feat: 중앙값 구하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -8,19 +8,19 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            String str = sc.next();
-            int number = sc.nextInt();
-            System.out.println(solution(str, number));
+            int index = sc.nextInt();
+            int[] arr = new int[index];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(solution(arr));
         }
     }
 
-    public static String solution(String my_string, int n) {
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            for (int j = 0; j < n; j++) {
-                answer += my_string.charAt(i);
-            }
-        }
+    public static int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        answer = array[array.length / 2];
         return answer;
     }
 }
