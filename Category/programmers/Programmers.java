6991fc74500ba7c3e1b8e53c100462963985d204
eom-tae-in <eti0728@daum.net>
2022-12-26 20:14:57 +0900

    Feat: 짝수는 싫어요

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,6 @@
 package Category.programmers;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -7,16 +8,21 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            System.out.println(solution(n));
+            int number = sc.nextInt();
+            System.out.println(Arrays.toString(solution(number)));
         }
     }
 
-    public static int solution(int n) {
-        int answer = 0;
-        String str = Integer.toString(n);
-        for (int i = 0; i < str.length(); i++) {
-            answer += str.charAt(i) - '0';
+    public static int[] solution(int n) {
+        int count = 0;
+        if (n % 2 == 0) {
+            count = n / 2;
+        } else {
+            count = n / 2 + 1;
+        }
+        int[] answer = new int[count];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = 2 * i + 1;
         }
         return answer;
     }
