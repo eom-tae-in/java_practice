b2033d318cb4c915a9001159272c9d62e5d99ec6
eom-tae-in <eti0728@daum.net>
2022-12-27 19:17:45 +0900

    Feat: 배열의 유사도

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -8,21 +8,28 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            int number = sc.nextInt();
-            System.out.println(Arrays.toString(solution(number)));
+            int s1Index = sc.nextInt();
+            int s2Index = sc.nextInt();
+            String[] s1 = new String[s1Index];
+            String[] s2 = new String[s2Index];
+            for (int i = 0; i < s1.length; i++) {
+                s1[i] = sc.next();
+            }
+            for (int i = 0; i < s2.length; i++) {
+                s2[i] = sc.next();
+            }
+            System.out.println(solution(s1, s2));
         }
     }
 
-    public static int[] solution(int n) {
-        int count = 0;
-        if (n % 2 == 0) {
-            count = n / 2;
-        } else {
-            count = n / 2 + 1;
-        }
-        int[] answer = new int[count];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = 2 * i + 1;
+    public static int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (String value : s1) {
+            for (String s : s2) {
+                if (s.equals(value)) {
+                    answer++;
+                }
+            }
         }
         return answer;
     }
