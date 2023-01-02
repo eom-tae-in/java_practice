90fe6984413347280f60bfd7dbabbf661138b691
eom-tae-in <eti0728@daum.net>
2023-01-02 23:11:16 +0900

    Refactor: 7의 개수

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -22,7 +22,7 @@ public class Programmers {
         int answer = 0;
         for (int i = 0; i < array.length; i++) {
             String str = Integer.toString(array[i]);
-            for (int j = 0; j < str.length(); i++) {
+            for (int j = 0; j < str.length(); j++) {
                 if (str.charAt(j) == '7') {
                     answer++;
                 }
