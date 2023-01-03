8ca925d2ed7fc42386bc2cf73b2d9da6f534fa8b
eom-tae-in <eti0728@daum.net>
2023-01-03 22:52:51 +0900

    Feat: 가위 바위 보

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,33 +1,28 @@
 package Category.programmers;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args){
+        public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int index = sc.nextInt();
-            int[] arr = new int[index];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = sc.nextInt();
-            }
-            System.out.println(solution(arr));
+            String str = sc.next();
+            System.out.println(solution(str));
         }
-    }
 
-    public static int solution(int[] array) {
-        int answer = 0;
-        for (int i = 0; i < array.length; i++) {
-            String str = Integer.toString(array[i]);
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == '7') {
-                    answer++;
+        public static String solution(String rsp) {
+            String answer = "";
+            for (int i = 0; i < rsp.length(); i++) {
+                if (rsp.charAt(i) == '2') {
+                    answer += "0";
+                } else if (rsp.charAt(i) == '0') {
+                    answer += "5";
+                } else {
+                    answer += "2";
                 }
             }
+            return answer;
         }
-        return answer;
     }
 }
