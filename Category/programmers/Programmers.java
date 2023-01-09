26acb6462bb19c5cd06eb1306b6420d5e0bab55e
eom-tae-in <eti0728@daum.net>
2023-01-09 17:41:25 +0900

    Feat: 개미 군단

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -8,17 +8,17 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-            System.out.println(solution(num));
+            int hp = sc.nextInt();
+            System.out.println(solution(hp));
         }
 
-        public static int solution(int n) {
+        public static int solution(int hp) {
             int answer = 0;
-            if (Math.sqrt(n) % 1 == 0) {
-                answer = 1;
-            } else {
-                answer = 2;
+            if (hp >= 5) {
+                answer += hp / 5;
+                hp %= 5;
             }
+            answer += hp / 3 + hp % 3;
             return answer;
         }
     }
