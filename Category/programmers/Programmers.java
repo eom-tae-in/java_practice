861e3e2eead4aecf9f0f03cd2ef9c6547ad0851e
eom-tae-in <eti0728@daum.net>
2023-01-10 21:53:15 +0900

    Feat: 모음 제거

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,24 +1,30 @@
 package Category.programmers;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int hp = sc.nextInt();
-            System.out.println(solution(hp));
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str = br.readLine();
+            System.out.println(solution(str));
         }
 
-        public static int solution(int hp) {
-            int answer = 0;
-            if (hp >= 5) {
-                answer += hp / 5;
-                hp %= 5;
+        public static String solution(String my_string) {
+            String answer = "";
+            for (int i = 0; i < my_string.length(); i++) {
+                if (my_string.charAt(i) == 'a' ||
+                  my_string.charAt(i) == 'e' ||
+                  my_string.charAt(i) == 'i' ||
+                  my_string.charAt(i) == 'o' ||
+                  my_string.charAt(i) == 'u') {
+                    continue;
+                }
+                answer += my_string.charAt(i);
             }
-            answer += hp / 3 + hp % 3;
             return answer;
         }
     }
