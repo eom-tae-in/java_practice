18b0c79a5e2b017b3ba7a4135ab7831867225cec
eom-tae-in <eti0728@daum.net>
2023-01-11 10:01:02 +0900

    Feat: 숨어있는 숫자의 덧셈 (1)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,29 +1,22 @@
 package Category.programmers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String str = br.readLine();
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.next();
             System.out.println(solution(str));
         }
 
-        public static String solution(String my_string) {
-            String answer = "";
+        public static int solution(String my_string) {
+            int answer = 0;
             for (int i = 0; i < my_string.length(); i++) {
-                if (my_string.charAt(i) == 'a' ||
-                  my_string.charAt(i) == 'e' ||
-                  my_string.charAt(i) == 'i' ||
-                  my_string.charAt(i) == 'o' ||
-                  my_string.charAt(i) == 'u') {
-                    continue;
+                if (Character.isDigit(my_string.charAt(i))) {
+                    answer += my_string.charAt(i) - '0';
                 }
-                answer += my_string.charAt(i);
             }
             return answer;
         }
