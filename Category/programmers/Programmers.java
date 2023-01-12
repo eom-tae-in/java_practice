4258d99f3fa187923b45262aa76cb40a0820b935
eom-tae-in <eti0728@daum.net>
2023-01-12 15:46:38 +0900

    Feat: 약수 구하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,9 @@
 package Category.programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programmers {
@@ -7,16 +11,24 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String str = sc.next();
-            System.out.println(solution(str));
+            int n = sc.nextInt();
+            System.out.println(Arrays.toString(solution(n)));
         }
 
-        public static int solution(String my_string) {
-            int answer = 0;
-            for (int i = 0; i < my_string.length(); i++) {
-                if (Character.isDigit(my_string.charAt(i))) {
-                    answer += my_string.charAt(i) - '0';
+        public static int[] solution(int n) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    list.add(i);
+                    if (n / i != i) {
+                        list.add(n / i);
+                    }
                 }
+            }
+            Collections.sort(list);
+            int[] answer = new int[list.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = list.get(i);
             }
             return answer;
         }
