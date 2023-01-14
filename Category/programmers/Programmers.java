6c840cf76fe1e2572ae04f11a2bd1bacecdd2e7d
eom-tae-in <eti0728@daum.net>
2023-01-14 22:20:26 +0900

    Feat: 숫자 찾기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,25 +1,30 @@
 package Category.programmers;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String my_string = br.readLine();
-            int num1 = Integer.parseInt(br.readLine());
-            int num2 = Integer.parseInt(br.readLine());
-            System.out.println(solution(my_string, num1, num2));
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            int k = sc.nextInt();
+            System.out.println(solution(num, k));
         }
 
-        public static String solution(String my_string, int num1, int num2) {
-            String answer = my_string.substring(0, num1) + my_string.charAt(num2) +
-              my_string.substring(num1+1, num2) + my_string.charAt(num1) + my_string.substring(num2+1);
+        public static int solution(int num, int k) {
+            int answer = 0;
+            String number = Integer.toString(num);
+            for (int i = 0; i < number.length(); i++) {
+                if (number.charAt(i) - '0' == k) {
+                    answer = i + 1;
+                    break;
+                }
+            }
+            if (answer == 0) {
+                answer = -1;
+            }
             return answer;
         }
     }
