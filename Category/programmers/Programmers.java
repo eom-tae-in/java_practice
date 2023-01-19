549c12a5780982ce98efeae322c6c20d19a135b3
eom-tae-in <eti0728@daum.net>
2023-01-19 21:33:41 +0900

    Feat: 피자 나눠 먹기 (2)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,6 +1,5 @@
 package Category.programmers;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -8,16 +7,17 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int age = sc.nextInt();
-            System.out.println(solution(age));
+            int peopleNumber = sc.nextInt();
+            System.out.println(solution(peopleNumber));
         }
 
-        public static String solution(int age) {
-            String answer = "";
-            String str = Integer.toString(age);
-            for (int i = 0; i < str.length(); i++) {
-                answer += Character.toString(str.charAt(i) + (char) 49);
+        public static int solution(int n) {
+            int answer = 0;
+            int pizzaPiece = 6;
+            while (pizzaPiece % n != 0) {
+                pizzaPiece += 6;
             }
+            answer = pizzaPiece / 6;
             return answer;
         }
     }
