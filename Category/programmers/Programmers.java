51a0795abe64d6e01f153b6b73d677a93c9b93fa
eom-tae-in <eti0728@daum.net>
2023-01-21 20:17:10 +0900

    Feat: 최댓값 만들기 (2)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,6 @@
 package Category.programmers;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -7,13 +8,27 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            for (int i = 1; i <= n; i++) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
+            int index = sc.nextInt();
+            int[] numbers = new int[index];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = sc.nextInt();
             }
+            System.out.println(solution(numbers));
+        }
+        public static int solution(int[] numbers) {
+            int answer = 0;
+            Arrays.sort(numbers);
+            if (numbers[1] < 0) {
+                if (numbers.length < 4) {
+                    answer = numbers[0] * numbers[1];
+                } else {
+                    answer = Math.max(numbers[0] * numbers[1],
+                      numbers[numbers.length - 2] * numbers[numbers.length - 1]);
+                }
+            } else {
+                answer = numbers[numbers.length - 2] * numbers[numbers.length - 1];
+            }
+            return answer;
         }
     }
 }
