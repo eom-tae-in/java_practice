bd5bc0c7b49037f518148ef9f55546f4b625403f
eom-tae-in <eti0728@daum.net>
2023-01-23 20:50:58 +0900

    Refactor: 중복된 문자 제거

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,22 +1,23 @@
 package Category.programmers;
 
-
-import java.util.HashSet;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            String str = sc.next();
-            System.out.println(solution(str));
-
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String my_string = br.readLine();
+            System.out.println(solution(my_string));
         }
         public static String solution(String my_string) {
             String answer = "";
-            HashSet<String> string = new HashSet<>();
-            string.add(my_string);
+            for (int i = 0; i < my_string.length(); i++) {
+                if (my_string.indexOf(my_string.charAt(i)) == i) {
+                    answer += my_string.charAt(i);
+                }
+            }
             return answer;
         }
     }
