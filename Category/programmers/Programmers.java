6c6bc9b5e871078c464ca2487815ecca0fa17047
eom-tae-in <eti0728@daum.net>
2023-01-24 20:52:16 +0900

    Feat: 암호 해독

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,22 +1,20 @@
 package Category.programmers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String my_string = br.readLine();
-            System.out.println(solution(my_string));
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String cipher = sc.next();
+            int code = sc.nextInt();
+            System.out.println(solution(cipher, code));
         }
-        public static String solution(String my_string) {
+        public static String solution(String cipher, int code) {
             String answer = "";
-            for (int i = 0; i < my_string.length(); i++) {
-                if (my_string.indexOf(my_string.charAt(i)) == i) {
-                    answer += my_string.charAt(i);
-                }
+            for (int i = code - 1; i < cipher.length(); i += code) {
+                answer += cipher.charAt(i);
             }
             return answer;
         }
