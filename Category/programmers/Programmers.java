2ef2d5aaa0e29001583cc6ee57fefdd5e6c16817
eom-tae-in <eti0728@daum.net>
2023-01-25 20:49:58 +0900

    Feat: 진료 순서 정하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,20 +1,35 @@
 package Category.programmers;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) {
+        public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            String cipher = sc.next();
-            int code = sc.nextInt();
-            System.out.println(solution(cipher, code));
+            int index = sc.nextInt();
+            int[] emergency = new int[index];
+            for (int i = 0; i < emergency.length; i++) {
+                emergency[i] = sc.nextInt();
+            }
+            System.out.println(Arrays.toString(solution(emergency)));
         }
-        public static String solution(String cipher, int code) {
-            String answer = "";
-            for (int i = code - 1; i < cipher.length(); i += code) {
-                answer += cipher.charAt(i);
+        public static int[] solution(int[] emergency) {
+            int[] answer = new int[emergency.length];
+            int[] reverse = new int[emergency.length];
+            for (int i = 0; i < reverse.length; i++) {
+                reverse[i] = emergency[i];
+            }
+            Arrays.sort(reverse);
+            int number = emergency.length;
+            for (int i = 0; i < reverse.length; i++) {
+                for (int j = 0; j < emergency.length; j++) {
+                    if (reverse[i] == emergency[j]) {
+                        answer[j] = number;
+                        number--;
+                    }
+                }
             }
             return answer;
         }
