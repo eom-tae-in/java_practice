2d5a089b35dbd560ea6b42d803cf0b8b3fcf4315
eom-tae-in <eti0728@daum.net>
2023-01-26 23:13:22 +0900

    Feat: 종이 자르기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -8,29 +8,19 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            int index = sc.nextInt();
-            int[] emergency = new int[index];
-            for (int i = 0; i < emergency.length; i++) {
-                emergency[i] = sc.nextInt();
-            }
-            System.out.println(Arrays.toString(solution(emergency)));
+            int M = sc.nextInt();
+            int N = sc.nextInt();
+            System.out.println(solution(M, N));
         }
-        public static int[] solution(int[] emergency) {
-            int[] answer = new int[emergency.length];
-            int[] reverse = new int[emergency.length];
-            for (int i = 0; i < reverse.length; i++) {
-                reverse[i] = emergency[i];
+        public static int solution(int M, int N) {
+            int answer = 0;
+            int piece = 1;
+            while (M != 1) {
+                answer += 1;
+                piece += 1;
+                M--;
             }
-            Arrays.sort(reverse);
-            int number = emergency.length;
-            for (int i = 0; i < reverse.length; i++) {
-                for (int j = 0; j < emergency.length; j++) {
-                    if (reverse[i] == emergency[j]) {
-                        answer[j] = number;
-                        number--;
-                    }
-                }
-            }
+            answer += piece * (N - 1);
             return answer;
         }
     }
