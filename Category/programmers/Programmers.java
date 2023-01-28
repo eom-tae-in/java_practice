014f2d65d0300bc0369461d5bff9e5b62ec0510a
eom-tae-in <eti0728@daum.net>
2023-01-28 22:40:05 +0900

    Feat: 문자열 정렬하기 (1)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,9 @@
 package Category.programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programmers {
@@ -8,13 +12,20 @@ public class Programmers {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             String my_string = sc.next();
-            System.out.println(solution(my_string));
+            System.out.println(Arrays.toString(solution(my_string)));
         }
-        public static String solution(String my_string) {
-            String answer = "";
+
+        public static int[] solution(String my_string) {
+            List<Integer> list = new ArrayList<>();
             for (int i = 0; i < my_string.length(); i++) {
-                answer += (my_string.charAt(i) < 'a') ? String.valueOf(my_string.charAt(i)).toLowerCase() :
-                  String.valueOf(my_string.charAt(i)).toUpperCase();
+                if (Character.isDigit(my_string.charAt(i))) {
+                    list.add(my_string.charAt(i) - '0');
+                }
+            }
+            Collections.sort(list);
+            int[] answer = new int[list.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = list.get(i);
             }
             return answer;
         }
