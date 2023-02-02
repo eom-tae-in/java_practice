dd0d3c90d1f3ea5afbf9e247cd88dea405556a5f
eom-tae-in <eti0728@daum.net>
2023-02-02 21:08:19 +0900

    Feat: 음양 더하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,6 +1,5 @@
 package Category.programmers;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -8,18 +7,26 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String s = sc.next();
-            System.out.println(solution(s));
-        }
-
-        public static String solution(String s) {
-            String answer = "";
-            if (s.length() % 2 == 0) {
-                answer += s.charAt(s.length() / 2 - 1);
-                answer += s.charAt(s.length() / 2);
-                return answer;
+            int index = sc.nextInt();
+            int[] absolute = new int[index];
+            for (int i = 0; i < absolute.length; i++) {
+                absolute[i] = sc.nextInt();
             }
-            answer += s.charAt(s.length() / 2);
+            boolean[] signs = new boolean[index];
+            for (int i = 0; i < signs.length; i++) {
+                signs[i] = sc.nextBoolean();
+            }
+            System.out.println(solution(absolute, signs));
+        }
+        public static int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+            for (int i = 0; i < absolutes.length; i++) {
+                if (signs[i]) {
+                    answer += absolutes[i];
+                    continue;
+                }
+                answer -= absolutes[i];
+            }
             return answer;
         }
     }
