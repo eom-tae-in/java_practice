d42c9a4a396844f08788f862dc592199e7f94bbf
eom-tae-in <eti0728@daum.net>
2023-02-03 22:19:21 +0900

    Feat: 문자열 내림차순으로 배치하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,5 +1,6 @@
 package Category.programmers;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programmers {
@@ -7,26 +8,14 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int index = sc.nextInt();
-            int[] absolute = new int[index];
-            for (int i = 0; i < absolute.length; i++) {
-                absolute[i] = sc.nextInt();
-            }
-            boolean[] signs = new boolean[index];
-            for (int i = 0; i < signs.length; i++) {
-                signs[i] = sc.nextBoolean();
-            }
-            System.out.println(solution(absolute, signs));
+            String s = sc.next();
+            System.out.println(solution(s));
         }
-        public static int solution(int[] absolutes, boolean[] signs) {
-            int answer = 0;
-            for (int i = 0; i < absolutes.length; i++) {
-                if (signs[i]) {
-                    answer += absolutes[i];
-                    continue;
-                }
-                answer -= absolutes[i];
-            }
+        public static String solution(String s) {
+            String answer = "";
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            answer = new StringBuilder(String.valueOf(charArray)).reverse().toString();
             return answer;
         }
     }
