ad2ffcaca1ecfac97f73c5e28e22a1f7f7c797be
eom-tae-in <eti0728@daum.net>
2023-02-05 20:28:24 +0900

    Feat: 직사각형 별찍기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -7,23 +7,14 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String t = sc.next();
-            String p = sc.next();
-            System.out.println(solution(t, p));
-        }
-
-        public static int solution(String t, String p) {
-            int answer = 0;
-            long pNumber = Long.parseLong(p);
-            int tryCount = t.length() - p.length();
-
-            for (int i = 0; i <= tryCount; i++) {
-                long tNumber = Long.parseLong(t.substring(i, i+ p.length()));
-                if (tNumber <= pNumber) {
-                    answer++;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for (int i = 0; i < b; i++) {
+                for (int j = 0; j < a; j++) {
+                    System.out.print("*");
                 }
+                System.out.println("");
             }
-            return answer;
         }
     }
 }
