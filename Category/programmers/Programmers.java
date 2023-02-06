9b8bf195c3ee676a4f1a06922232beeb69a16485
eom-tae-in <eti0728@daum.net>
2023-02-06 22:47:21 +0900

    Feat: 문자열 다루기 기본

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -7,14 +7,24 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            for (int i = 0; i < b; i++) {
-                for (int j = 0; j < a; j++) {
-                    System.out.print("*");
-                }
-                System.out.println("");
+            String s = sc.next();
+            System.out.println(solution(s));
+        }
+
+        public static boolean solution(String s) {
+            boolean answer = true;
+            int stringLength = s.length();
+            if (!(stringLength == 4 || stringLength == 6)) {
+                answer = false;
+                return answer;
             }
+            for (int i = 0; i < stringLength; i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    answer = false;
+                    return answer;
+                }
+            }
+            return answer;
         }
     }
 }
