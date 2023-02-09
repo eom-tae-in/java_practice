53ae77e2142e5fbd9a15f9549afa0c1b95696c2b
eom-tae-in <eti0728@daum.net>
2023-02-10 00:32:53 +0900

    Feat: 중복된 문자 제거

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,33 +1,32 @@
 package Category.programmers;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
 
 public class Programmers {
 
     static class Solution {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            System.out.println(solution(n));
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String my_string = br.readLine();
+            System.out.println(solution(my_string));
         }
 
-        public static int solution(int n) {
-            int answer = 0;
-            for (int i = 4; i <= n; i++) {
-                if (compositionNumber(i)) {
-                    answer++;
-                }
-            }
-            return answer;
+        public static String solution(String my_string) {
+            return deduplicate(my_string);
         }
 
-        public static boolean compositionNumber(int number) {
-            for (int i = 2; i * i <= number; i++) {
-                if (number % i == 0) {
-                    return true;
-                }
+        public static String deduplicate(String my_string) {
+            LinkedHashSet<String> deduplication = new LinkedHashSet<>();
+            for (int i = 0; i < my_string.length(); i++) {
+                deduplication.add(String.valueOf(my_string.charAt(i)));
             }
-            return false;
+            String[] deduplicatedArray = new String[deduplication.size()];
+            deduplicatedArray = deduplication.toArray(deduplicatedArray);
+            return String.join("", deduplicatedArray);
         }
+
     }
 }
