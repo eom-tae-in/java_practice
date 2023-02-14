102843ce3426f98ce828904e2ae2ccf77ebfde22
eom-tae-in <eti0728@daum.net>
2023-02-14 22:31:00 +0900

    Feat: 한 번만 등장한 문자

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,27 +1,42 @@
 package Category.programmers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Programmers {
 
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int indexLength = sc.nextInt();
-            int[] num_list = new int[indexLength];
-            for (int i = 0; i < num_list.length; i++) {
-                num_list[i] = sc.nextInt();
-            }
-            int n = sc.nextInt();
-            System.out.println(Arrays.deepToString(solution(num_list, n)));
+            String s = sc.next();
+            System.out.println(solution(s));
         }
 
-        public static int[][] solution(int[] num_list, int n) {
-            int[][] answer = new int[num_list.length / n][n];
-            for (int i = 0; i < num_list.length / n; i++) {
-                System.arraycopy(num_list, i * n, answer[i], 0, n);
+        public static String solution(String s) {
+            String answer = "";
+            List<String> list = new ArrayList<>();
+            Set<String> set = new HashSet<>();
+            for (int i = 0; i < s.length(); i++) {
+                set.add(String.valueOf(s.charAt(i)));
+                System.out.println(set);
             }
+            for (int i = 0; i < s.length(); i++) {
+                if (list.contains(String.valueOf(s.charAt(i)))) {
+                    set.remove(String.valueOf(s.charAt(i)));
+                    continue;
+                }
+                list.add(String.valueOf(s.charAt(i)));
+            }
+            System.out.println(set);
+            String[] str = set.toArray(new String[0]);
+            Arrays.sort(str);
+            answer = String.join("", str);
             return answer;
         }
     }
