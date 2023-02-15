6a8303fbba456151f3526c0ca0d9fa7007553d35
eom-tae-in <eti0728@daum.net>
2023-02-15 20:24:37 +0900

    Feat: 숨어있는 숫자의 덧셈 (2)

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -1,42 +1,26 @@
 package Category.programmers;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Programmers {
 
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String s = sc.next();
-            System.out.println(solution(s));
+            String my_stirng = sc.next();
+            System.out.println(solution(my_stirng));
         }
 
-        public static String solution(String s) {
-            String answer = "";
-            List<String> list = new ArrayList<>();
-            Set<String> set = new HashSet<>();
-            for (int i = 0; i < s.length(); i++) {
-                set.add(String.valueOf(s.charAt(i)));
-                System.out.println(set);
-            }
-            for (int i = 0; i < s.length(); i++) {
-                if (list.contains(String.valueOf(s.charAt(i)))) {
-                    set.remove(String.valueOf(s.charAt(i)));
+        public static int solution(String my_string) {
+            int answer = 0;
+            String[] numbers = my_string.split("[a-zA-Z]");
+
+            for (String number : numbers) {
+                if (number.equals("")) {
                     continue;
                 }
-                list.add(String.valueOf(s.charAt(i)));
+                answer += Integer.parseInt(number);
             }
-            System.out.println(set);
-            String[] str = set.toArray(new String[0]);
-            Arrays.sort(str);
-            answer = String.join("", str);
             return answer;
         }
     }
