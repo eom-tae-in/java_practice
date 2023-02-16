235fd50ccbb38e507aac14fd947a16f556bebeef
eom-tae-in <eti0728@daum.net>
2023-02-16 23:26:42 +0900

    Feat: 이진수 더하기

--- Category/programmers/Programmers.java
+++ Category/programmers/Programmers.java
@@ -7,21 +7,12 @@ public class Programmers {
     static class Solution {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            String my_stirng = sc.next();
-            System.out.println(solution(my_stirng));
+            String bin1 = sc.next();
+            String bin2 = sc.next();
+            System.out.println(solution(bin1, bin2));
         }
-
-        public static int solution(String my_string) {
-            int answer = 0;
-            String[] numbers = my_string.split("[a-zA-Z]");
-
-            for (String number : numbers) {
-                if (number.equals("")) {
-                    continue;
-                }
-                answer += Integer.parseInt(number);
-            }
-            return answer;
+        public static String solution(String bin1, String bin2) {
+            return Integer.toBinaryString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2));
         }
     }
 }
