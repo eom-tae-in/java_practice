55b1648b493a3a7d54fb8872be7aa454ae8101e8
eom-tae-in <eti0728@daum.net>
2023-03-24 07:39:02 +0900

    Feat: 주식가격

---
+++ Category/programmers/stockPrice.java
@@ -0,0 +1,44 @@
+package Category.programmers;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class stockPrice {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] prices = new int[num];
+        for (int i = 0; i < num; i++) {
+            prices[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(solution(prices)));
+    }
+
+    public static int[] solution(int[] prices) {
+        int num = 0;
+        int[] answer = new int[prices.length];
+        Queue<Integer> queue = new LinkedList<>();
+        for (int price : prices) {
+            queue.add(price);
+        }
+        while (!queue.isEmpty()) {
+            int count = queue.remove();
+            int n = 0;
+            for (int i : queue) {
+                n++;
+                if (count > i) {
+                    answer[num] = n;
+                    break;
+                }
+            }
+            if (n == queue.size()) {
+                answer[num] = queue.size();
+            }
+            num++;
+        }
+        return answer;
+    }
+}
