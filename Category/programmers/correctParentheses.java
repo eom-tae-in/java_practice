7a5553564b0ddb90ba091e01a8af4189ee1324a6
eom-tae-in <eti0728@daum.net>
2023-03-25 16:57:49 +0900

    feat: 올바를 괄호

---
+++ Category/programmers/correctParentheses.java
@@ -0,0 +1,30 @@
+package Category.programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class correctParentheses {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                queue.add(c);
+            }
+            else if (c == ')') {
+                if (queue.isEmpty()) {
+                    return false;
+                }
+                queue.remove();
+            }
+        }
+        return queue.isEmpty();
+    }
+}
