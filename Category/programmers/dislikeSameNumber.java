9b83d4728b26599e26c1d44a833e1478fb396e96
eom-tae-in <eti0728@daum.net>
2023-03-27 23:45:46 +0900

    feat: 같은 숫자는 싫어

---
+++ Category/programmers/dislikeSameNumber.java
@@ -0,0 +1,27 @@
+package Category.programmers;
+
+import java.util.*;
+
+public class dislikeSameNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+
+    public static int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (stack.peek() != arr[i]) {
+                stack.add(arr[i]);
+            }
+        }
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
