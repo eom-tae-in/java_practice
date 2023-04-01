7e3be4530072dcbaae7fab7290e19d2e05f374bd
eom-tae-in <eti0728@daum.net>
2023-04-02 00:36:45 +0900

    [refactor] q_1326

--- Category/baekjoon/q_1326.java
+++ Category/baekjoon/q_1326.java
@@ -9,21 +9,28 @@ public class q_1326 {
     static class Board {
 
         int position;
+        int value;
         int sum;
-        public Board(int position, int sum) {
+
+        public Board(int position, int value, int sum) {
             this.position = position;
+            this.value = value;
             this.sum = sum;
         }
     }
+    static boolean[] ch;
     static int total = Integer.MAX_VALUE;
+
+    static int n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] bridge = new int[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        n = sc.nextInt();
+        int[] bridge = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
             bridge[i] = sc.nextInt();
         }
+        ch = new boolean[n + 1];
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println(bfs(bridge, a, b));
@@ -31,24 +38,25 @@ public class q_1326 {
 
     public static int bfs(int[] bridge, int a, int b) {
         Queue<Board> queue = new LinkedList<>();
-        queue.add(new Board(a, 0));
+        queue.add(new Board(a, bridge[a], 0));
+        ch[a] = true;
 
         while (!queue.isEmpty()) {
             Board board = queue.remove();
-
             if (board.position == b) {
                 total = Math.min(board.sum, total);
+                break;
             }
-            for (int i = board.position + 1; i <= b; i++) {
-                if ((i - board.position) % bridge[board.position] == 0) {
-                    queue.add(new Board(i, board.sum + 1));
+            for (int i = 1; i < bridge.length; i++) {
+                if (Math.abs(i - board.position) % bridge[board.position] == 0 && !ch[i]) {
+                    ch[i] = true;
+                    queue.add(new Board(i, bridge[i], board.sum + 1));
                 }
             }
         }
         if (total == Integer.MAX_VALUE) {
             return -1;
         }
-
         return total;
     }
 }
