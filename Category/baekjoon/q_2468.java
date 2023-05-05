65f1cbbd25af813128910a0ab35c84bb8e80402f
eom-tae-in <eti0728@daum.net>
2023-05-06 00:18:51 +0900

    [refactor] q_2468

--- Category/baekjoon/q_2468.java
+++ Category/baekjoon/q_2468.java
@@ -5,39 +5,27 @@ import java.util.Scanner;
 public class q_2468 {
 
     static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-
+    static int[] dy = {-1, 1, 0, 0};
     static int maxHeight;
-
-    static boolean[][] calculate;
-
     static boolean[][] check;
-
     static int[][] array;
-
     static int N;
 
-
-    public static void dfs(int x, int y, int height) {
-        check[x][y] = true;
+    public static void dfs(int y, int x, int height) {
+        check[y][x] = true;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
             int ny = y + dy[i];
-            if ((nx >= 0 && nx < N && ny >= 0 && ny < N) && !check[nx][ny] && array[nx][ny] >= height) {
-                dfs(nx, ny, height);
+            int nx = x + dx[i];
+            if ((nx >= 0 && nx < N && ny >= 0 && ny < N) && !check[ny][nx] && array[ny][nx] >= height) {
+                dfs(ny, nx, height);
             }
         }
     }
-
-
-
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         array = new int[N][N];
-        calculate = new boolean[N][N];
         maxHeight = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
@@ -49,8 +37,9 @@ public class q_2468 {
         }
         int max = 0;
         int count = 0;
-        for (int height = 2; height <= maxHeight; height++) {
+        for (int height = 1; height <= maxHeight; height++) {
             check = new boolean[N][N];
+            count = 0;
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
                     if (array[i][j] >= height && !check[i][j]) {
