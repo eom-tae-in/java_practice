444a5f1e1ec493e75b783f9cf3d52a1ca759446f
eom-tae-in <eti0728@daum.net>
2023-06-05 23:55:05 +0900

    [feat] q_1152

---
+++ Category/baekjoon/q_1152.java
@@ -0,0 +1,17 @@
+package Category.baekjoon;
+
+import java.util.Scanner;
+
+public class q_1152 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().trim();
+        if (str.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        String[] strings = str.split(" ");
+        System.out.println(strings.length);
+    }
+}
