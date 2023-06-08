50a3a6ed3a752d366c715c723e036a17769eeb2e
eom-tae-in <eti0728@daum.net>
2023-06-08 18:59:46 +0900

    [feat] q_2438

---
+++ Category/baekjoon/q_2438.java
@@ -0,0 +1,15 @@
+package Category.baekjoon;
+
+import java.util.Scanner;
+
+public class q_2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            sb.append("*");
+            System.out.println(sb);
+        }
+    }
+}
