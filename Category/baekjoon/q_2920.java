fe04eb14325f1133b39e2408c099c8d65f3921b4
eom-tae-in <eti0728@daum.net>
2023-06-11 23:23:47 +0900

    [feat] q_2920

--- Category/baekjoon/q_2920.java
+++ Category/baekjoon/q_2920.java
@@ -1,22 +1,26 @@
 package Category.baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class q_2920 {
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int count = sc.nextInt();
-            String str = sc.next();
-            for (int j = 0; j < str.length(); j++) {
-                sb.append(String.valueOf(str.charAt(j)).repeat(count));
-            }
-            sb.append("\n");
+        int[] input = new int[8];
+        int[] ascending = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] descending = {8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 0; i < input.length; i++) {
+            input[i] = sc.nextInt();
         }
-        System.out.println(sb);
+        if (IntStream.range(0, input.length).allMatch(i -> input[i] == ascending[i])) {
+            System.out.println("ascending");
+            return;
+        }
+        if (IntStream.range(0, input.length).allMatch(i -> input[i] == descending[i])) {
+            System.out.println("descending");
+            return;
+        }
+        System.out.println("mixed");
     }
 }
