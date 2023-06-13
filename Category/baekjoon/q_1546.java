1a9d3bdba12b9e133e900a5f31f14e3fb8fdcd78
eom-tae-in <eti0728@daum.net>
2023-06-14 00:01:00 +0900

    q_1546

--- Category/baekjoon/q_1546.java
+++ Category/baekjoon/q_1546.java
@@ -6,7 +6,7 @@ public class q_1546 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] scores = new int[n];
+        double[] scores = new double[n];
         double max = 0;
         for (int i = 0; i < n; i++) {
             scores[i] = sc.nextInt();
