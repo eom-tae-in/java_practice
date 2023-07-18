f991b5150607e33d29fa1c30123557642b459b95
eom-tae-in <eti0728@daum.net>
2023-07-18 13:31:00 +0900

    [feat] July18rd

---
+++ Category/leetcode/July18rd.java
@@ -0,0 +1,28 @@
+package Category.leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class July18rd {
+    class LRUCache {
+
+        private final Map<Integer, Integer> map;
+
+        public LRUCache(final int capacity) {
+            map = new LinkedHashMap<>(capacity, 0.75f, true) {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry eldest) {
+                    return size() > capacity;
+                }
+            };
+        }
+
+        public int get(final int key) {
+            return map.getOrDefault(key, -1);
+        }
+
+        public void put(final int key, final int value) {
+            map.put(key, value);
+        }
+    }
+}
