c5032703741de4c15f182cae43fbcff2a4271791
eom-tae-in <eti0728@daum.net>
2023-09-24 19:26:29 +0900

    [feat] solve september 24th

---
+++ category/leetcode/september/twenty_four/September24th.java
@@ -0,0 +1,21 @@
+package category.leetcode.september.twenty_four;
+
+public class September24th {
+    class Solution {
+        public double champagneTower(int poured, int query_row, int query_glass) {
+            double[][] glasses = new double[query_row + 1][query_row + 1];
+            glasses[0][0] = poured;
+            for (int i = 0; i < query_row; ++i) {
+                for (int j = 0; j <= i; ++j) {
+                    if (glasses[i][j] > 1) {
+                        glasses[i + 1][j] += (glasses[i][j] - 1) / 2.0;
+                        glasses[i + 1][j + 1] += (glasses[i][j] - 1) / 2.0;
+                    }
+                }
+            }
+
+            return Math.min(1.0, glasses[query_row][query_glass]);
+        }
+    }
+
+}
