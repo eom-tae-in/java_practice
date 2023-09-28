7ddba058ac852be31d63b93d027edfe0930b6c29
eom-tae-in <eti0728@daum.net>
2023-09-28 16:49:46 +0900

    [feat] solve semptember 28th

---
+++ category/leetcode/september/twenty_eighth/September28th.java
@@ -0,0 +1,23 @@
+package category.leetcode.september.twenty_eighth;
+
+public class September28th {
+
+    public int[] sortArrayByParity(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            if (nums[l] % 2 == 1 && nums[r] % 2 == 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+            }
+            if (nums[l] % 2 == 0) {
+                l++;
+            }
+            if (nums[r] % 2 == 1) {
+                r--;
+            }
+        }
+        return nums;
+    }
+}
