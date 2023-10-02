8a6066ca31edfc268a6791d0f3fcfbcd6141c5dd
eom-tae-in <eti0728@daum.net>
2023-10-03 00:10:39 +0900

    [feat] solve October 1st

---
+++ category/leetcode/october/first/October1st.java
@@ -0,0 +1,28 @@
+package category.leetcode.october.first;
+
+public class October1st {
+
+    public String reverseWords(String s) {
+        char[] charArr = s.toCharArray();
+        int point = 0;
+
+        for (int i = 0; i < charArr.length; i++) {
+            if (i == charArr.length - 1) {
+                reverse(charArr, point, i);
+            }
+            if (charArr[i] == ' ') {
+                reverse(charArr, point, i - 1);
+                point = i + 1;
+            }
+        }
+        return new String(charArr);
+    }
+
+    private void reverse(char[] charArr, int start, int end) {
+        while (start < end) {
+            char temp = charArr[start];
+            charArr[start++] = charArr[end];
+            charArr[end--] = temp;
+        }
+    }
+}
