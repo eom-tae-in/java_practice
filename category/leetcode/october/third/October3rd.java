7dec6ea52b9bea5e7b3eb957b3908022a3261a63
eom-tae-in <eti0728@daum.net>
2023-10-03 10:10:46 +0900

    [feat] solve October 3rd

---
+++ category/leetcode/october/third/October3rd.java
@@ -0,0 +1,19 @@
+package category.leetcode.october.third;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class October3rd {
+
+    public int numIdenticalPairs(int[] nums) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            int current = map.getOrDefault(num, 0);
+            answer += current;
+            map.put(num, current + 1);
+        }
+        return answer;
+    }
+}
