845178db4b1eb4be648396864c3e614740598dfb
eom-tae-in <eti0728@daum.net>
2023-10-11 22:38:47 +0900

    [feat] solve October 6th

---
+++ category/leetcode/october/sixth/October6th.java
@@ -0,0 +1,15 @@
+package category.leetcode.october.sixth;
+
+public class October6th {
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i < n + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+}
