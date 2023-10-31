a2fd68a69683de09159cecd02251547a67bab480
eom-tae-in <eti0728@daum.net>
2023-10-31 23:15:01 +0900

    [feat] solve Octover 31st

---
+++ category/leetcode/october/October31st.java
@@ -0,0 +1,13 @@
+package category.leetcode.october;
+
+public class October31st {
+
+    public int[] findArray(int[] pref) {
+        int[] answer = new int[pref.length];
+        answer[0] = pref[0];
+        for (int i = 1; i < pref.length; i++) {
+            answer[i] = pref[i] ^ pref[i - 1];
+        }
+        return answer;
+    }
+}
