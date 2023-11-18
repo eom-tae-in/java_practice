e6a4a09ccc3752b47481927777414c599c1bf30b
eom-tae-in <eti0728@daum.net>
2023-11-19 06:25:42 +0900

    [feat] solve November 17th

---
+++ category/leetcode/november/November17th.java
@@ -0,0 +1,17 @@
+package category.leetcode.november;
+
+import java.util.Arrays;
+
+public class November17th {
+
+    public int minPairSum(int[] nums) {
+        int ans = 0;
+        Arrays.sort(nums);
+
+        for (int l = 0, r = nums.length - 1; l < r; ) {
+            ans = Math.max(ans, nums[l++] + nums[r--]);
+        }
+
+        return ans;
+    }
+}
