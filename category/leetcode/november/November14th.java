1cd4d9e886b8042537d9c68afaafbc282ded6db2
eom-tae-in <eti0728@daum.net>
2023-11-19 10:28:44 +0900

    [feat] solve November 14th

---
+++ category/leetcode/november/November14th.java
@@ -0,0 +1,30 @@
+package category.leetcode.november;
+
+import java.util.Arrays;
+
+public class November14th {
+
+    public int countPalindromicSubsequence(String s) {
+        int sum = 0;
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            first[index] = Math.min(first[index], i);
+            last[index] = i;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (first[i] < last[i]) {
+                sum += s.substring(first[i] + 1, last[i])
+                        .chars()
+                        .distinct()
+                        .count();
+            }
+        }
+
+        return sum;
+    }
+}
