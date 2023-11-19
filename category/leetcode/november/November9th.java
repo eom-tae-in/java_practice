47a11c6b1918a0251ce3d48fd67d0ade0c92f8e1
eom-tae-in <eti0728@daum.net>
2023-11-19 23:07:32 +0900

    [feat] solve November 9th

---
+++ category/leetcode/november/November9th.java
@@ -0,0 +1,20 @@
+package category.leetcode.november;
+
+public class November9th {
+
+    public int countHomogenous(String s) {
+        int mod = 1_000_000_007;
+        int ans = 0;
+        int count = 0;
+        char current = 'A';
+
+        for (char ch : s.toCharArray()) {
+            count = current == ch ? count + 1 : 1;
+            current = ch;
+            ans += count;
+            ans %= mod;
+        }
+
+        return ans;
+    }
+}
