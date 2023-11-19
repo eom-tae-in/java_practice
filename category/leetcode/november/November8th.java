b620be1c4a3ebe73d69e8afdb8b3b18c6b878b06
eom-tae-in <eti0728@daum.net>
2023-11-19 23:20:11 +0900

    [feat] solve November 8th

---
+++ category/leetcode/november/November8th.java
@@ -0,0 +1,10 @@
+package category.leetcode.november;
+
+public class November8th {
+
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int minCase = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
+
+        return minCase == 0 ? t != 1 : minCase <= t;
+    }
+}
