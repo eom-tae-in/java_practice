516651cc55ce2bb57aa4b5939863c41a25809c9b
eom-tae-in <eti0728@daum.net>
2023-11-20 00:02:23 +0900

    [feat] solve November 4th

---
+++ category/leetcode/november/November4th.java
@@ -0,0 +1,22 @@
+package category.leetcode.november;
+
+public class November4th {
+
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int max = -1;
+
+        for (int l : left) {
+            if (l > max) {
+                max = l;
+            }
+        }
+
+        for (int r : right) {
+            if (n - r > max) {
+                max = n - r;
+            }
+        }
+
+        return max;
+    }
+}
