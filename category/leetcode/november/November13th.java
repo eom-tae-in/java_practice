02d7124658aca6f8c14f73d80891785a5ba8e99a
eom-tae-in <eti0728@daum.net>
2023-11-19 11:06:49 +0900

    [feat] solve November 13th

---
+++ category/leetcode/november/November13th.java
@@ -0,0 +1,32 @@
+package category.leetcode.november;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class November13th {
+
+    public String sortVowels(String s) {
+        StringBuilder sb = new StringBuilder();
+        List<Character> vowels = new ArrayList<>();
+
+        for (char ch : s.toCharArray()) {
+            if (isVowel(ch)) {
+                vowels.add(ch);
+            }
+        }
+
+        Collections.sort(vowels);
+        int index = 0;
+
+        for (char ch : s.toCharArray()) {
+            sb.append(isVowel(ch) ? vowels.get(index++) : ch);
+        }
+
+        return sb.toString();
+    }
+
+    private boolean isVowel(final char ch) {
+        return "aeiouAEIOU".indexOf(ch) != -1;
+    }
+}
