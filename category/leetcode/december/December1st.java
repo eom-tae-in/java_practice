0481419b673d836323991cfe91f1399592dd3b5b
eom-tae-in <eti0728@daum.net>
2023-12-02 08:24:26 +0900

    [faet] solve December 1st

---
+++ category/leetcode/december/December1st.java
@@ -0,0 +1,22 @@
+package category.leetcode.december;
+
+public class December1st {
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String word : word1) {
+            stringBuilder.append(word);
+        }
+
+        String totalWord1 = stringBuilder.toString();
+        stringBuilder.setLength(0);
+
+        for (String word : word2) {
+            stringBuilder.append(word);
+        }
+
+        String totalWord2 = stringBuilder.toString();
+        return totalWord1.equals(totalWord2);
+    }
+}
