f6a288eafe70ff61de8f05dc2ba02565b5e05693
eom-tae-in <eti0728@daum.net>
2023-12-27 19:41:44 +0900

    [feat] solve December 27th

---
+++ category/leetcode/december/December27th.java
@@ -0,0 +1,20 @@
+package category.leetcode.december;
+
+public class December27th {
+
+    public int minCost(String colors, int[] neededTime) {
+        int ans = 0;
+        int maxNeededTime = neededTime[0];
+
+        for (int i = 1; i < colors.length(); ++i) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                ans += Math.min(maxNeededTime, neededTime[i]);
+                maxNeededTime = Math.max(maxNeededTime, neededTime[i]);
+            } else {
+                maxNeededTime = neededTime[i];
+            }
+        }
+
+        return ans;
+    }
+}
