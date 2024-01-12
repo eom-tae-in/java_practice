9e649d47cb318ecc3a81c9099b8bb26d76c10e44
eom-tae-in <eti0728@daum.net>
2024-01-12 20:22:57 +0900

    [feat] solve January 12th

---
+++ category/leetcode/_2024/January/January12th.java
@@ -0,0 +1,23 @@
+package category.leetcode._2024.January;
+
+public class January12th {
+
+    public boolean halvesAreAlike(final String s) {
+        final int halfLength = s.length() / 2;
+        String a = s.substring(0, halfLength);
+        String b = s.substring(halfLength);
+        final int aVowelCount = countVowel(a);
+        final int bVowelCount = countVowel(b);
+
+        return aVowelCount == bVowelCount;
+    }
+    private int countVowel(final String s) {
+        return (int) s.chars()
+                .filter(c -> isVowel((char) c))
+                .count();
+    }
+
+    private boolean isVowel(final char character) {
+        return "AEIOUaeiou".indexOf(character) != -1;
+    }
+}
