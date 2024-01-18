b60a0e3307f42ae78aa7bc68ae739c9e5cf2d613
eom-tae-in <eti0728@daum.net>
2024-01-18 23:57:08 +0900

    [feat] solve January 18th

---
+++ category/leetcode/_2024/January/January18th.java
@@ -0,0 +1,18 @@
+package category.leetcode._2024.January;
+
+public class January18th {
+
+    public int climbStairs(final int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n];
+    }
+}
+
+
