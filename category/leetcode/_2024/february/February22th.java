27a4880c0f4e878005a06e5bd791d0e531e1ddd8
eom-tae-in <eti0728@daum.net>
2024-02-22 20:26:50 +0900

    [feat] solve February 22th

---
+++ category/leetcode/_2024/february/February22th.java
@@ -0,0 +1,21 @@
+package category.leetcode._2024.february;
+
+public class February22th {
+
+    public int findJudge(final int n, final int[][] trust) {
+        int[] count = new int[n + 1];
+
+        for (int[] currentTrust : trust) {
+            count[currentTrust[0]]--;
+            count[currentTrust[1]]++;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (count[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
