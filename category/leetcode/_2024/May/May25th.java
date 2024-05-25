45307bac4169000b8f2a11431818743b4c48f096
eom-tae-in <eti0728@daum.net>
2024-05-25 16:08:16 +0900

    [feat] solve May 25

---
+++ category/leetcode/_2024/May/May25th.java
@@ -0,0 +1,40 @@
+package category.leetcode._2024.May;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class May25th {
+
+    public List<String> wordBreak(final String s, final List<String> wordDict) {
+        Map<String, List<String>> map = new HashMap<>();
+        return dfs(s, wordDict, map);
+    }
+
+    private List<String> dfs(final String s, final List<String> wordDict, final Map<String, List<String>> map) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+
+        List<String> subsets = new ArrayList<>();
+
+        for (int i = 1; i < s.length(); i++) {
+            String prefix = s.substring(0, i);
+            String suffix = s.substring(i);
+            if (wordDict.contains(prefix)) {
+                for (String subset : dfs(suffix, wordDict, map)) {
+                    subsets.add(prefix + " " + subset);
+                }
+            }
+        }
+
+        if (wordDict.contains(s)) {
+            subsets.add(s);
+        }
+
+        map.put(s, subsets);
+        return subsets;
+    }
+}
