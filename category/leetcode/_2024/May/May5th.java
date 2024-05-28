936c7b6d7ca3078f65d05a5e3faff157dd9ab276
eom-tae-in <eti0728@daum.net>
2024-05-28 21:40:09 +0900

    [refactor] refactor ListNode class

--- category/leetcode/_2024/May/May5th.java
+++ category/leetcode/_2024/May/May5th.java
@@ -12,7 +12,15 @@ class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
