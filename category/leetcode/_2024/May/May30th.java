d408afefdd39a8858e4d0e626987de82e64c6640
eom-tae-in <eti0728@daum.net>
2024-05-30 17:59:51 +0900

    [feat] solve May 30th

---
+++ category/leetcode/_2024/May/May30th.java
@@ -0,0 +1,19 @@
+package category.leetcode._2024.May;
+
+public class May30th {
+
+    public int countTriplets(final int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int xor = 0;
+            for (int j = i; j < nums.length; j++) {
+                xor ^= nums[j];
+                if (xor == 0) {
+                    count += (j - i);
+                }
+            }
+        }
+
+        return count;
+    }
+}
