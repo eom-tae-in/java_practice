87d7d0c12ddd5deaf087f9f82541d01d679c10a9
eom-tae-in <eti0728@daum.net>
2024-06-03 11:02:07 +0900

    [feat] solve June 3rd

---
+++ category/leetcode/_2024/June/June3rd.java
@@ -0,0 +1,20 @@
+package category.leetcode._2024.June;
+
+public class June3rd {
+
+    public int appendCharacters(final String s, final String t) {
+        int sIndex = 0;
+        int tIndex = 0;
+        int sLength = s.length();
+        int tLength = t.length();
+
+        while (sIndex < sLength && tIndex < tLength) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                tIndex++;
+            }
+            sIndex++;
+        }
+
+        return tLength - tIndex;
+    }
+}
