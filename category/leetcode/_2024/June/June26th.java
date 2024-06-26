05a4a356fc18ec55d4df3417bdfe2e96bdddc005
eom-tae-in <eti0728@daum.net>
2024-06-26 11:53:20 +0900

    [feat] solve June 26th

---
+++ category/leetcode/_2024/June/June26th.java
@@ -0,0 +1,31 @@
+package category.leetcode._2024.June;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class June26th {
+
+    public TreeNode balanceBST(final TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        orderByAsc(root, list);
+        return makeBalanceTree(list, 0, list.size() - 1);
+    }
+
+    private void orderByAsc(final TreeNode node, final List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        orderByAsc(node.left, list);
+        list.add(node.val);
+        orderByAsc(node.right, list);
+    }
+
+    private TreeNode makeBalanceTree(final List<Integer> list, final int left, final int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        return new TreeNode(list.get(mid), makeBalanceTree(list, left, mid - 1),
+                makeBalanceTree(list, mid + 1, right));
+    }
+}
