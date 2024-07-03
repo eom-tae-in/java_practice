6707f418b7501321aaa79e9206b1ad5bd9272bbc
eom-tae-in <eti0728@daum.net>
2024-07-03 11:11:15 +0900

    [feat] solve July 3rd

---
+++ category/leetcode/_2024/July/July3rd.java
@@ -0,0 +1,20 @@
+package category.leetcode._2024.July;
+
+import java.util.Arrays;
+
+public class July3rd {
+
+    public int minDifference(final int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int answer = Integer.MAX_VALUE;
+        if (n <= 4) {
+            return 0;
+        }
+        for (int i = 0; i <= 3; i++) {
+            answer = Math.min(answer, nums[n - 4 + i] - nums[i]);
+        }
+
+        return answer;
+    }
+}
