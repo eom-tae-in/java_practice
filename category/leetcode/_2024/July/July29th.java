aaae6a318184cf34be4f0450babcf9117d8927f2
eom-tae-in <eti0728@daum.net>
2024-07-29 09:57:37 +0900

    [feat] solve July 29th

---
+++ category/leetcode/_2024/July/July29th.java
@@ -0,0 +1,32 @@
+package category.leetcode._2024.July;
+
+public class July29th {
+    public int numTeams(final int[] rating) {
+        int count = 0;
+        int n = rating.length;
+        for (int i = 1; i < n - 1; i++) {
+            int current = rating[i];
+            int leftLess = 0;
+            int leftGreater = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (current > rating[j]) {
+                    leftLess++;
+                } else if (current < rating[j]) {
+                    leftGreater++;
+                }
+            }
+            int rightLess = 0;
+            int rightGreater = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (current > rating[j]) {
+                    rightLess++;
+                } else if (current < rating[j]) {
+                    rightGreater++;
+                }
+            }
+            count += leftLess * rightGreater + leftGreater * rightLess;
+        }
+
+        return count;
+    }
+}
