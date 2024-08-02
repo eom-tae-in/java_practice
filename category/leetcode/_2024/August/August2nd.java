bd3d67c22b5d30558ff97274a9697c8c805cfa12
eom-tae-in <eti0728@daum.net>
2024-08-02 11:05:24 +0900

    [feat] solve August 2nd

---
+++ category/leetcode/_2024/August/August2nd.java
@@ -0,0 +1,26 @@
+package category.leetcode._2024.August;
+
+import java.util.Arrays;
+
+public class August2nd {
+
+    public int minSwaps(final int[] nums) {
+        final int n = nums.length;
+        final int k = (int) Arrays.stream(nums)
+                .filter(a -> a == 1)
+                .count();
+        int ones = 0;
+        int maxOnes = 0;
+        for (int i = 0; i < n * 2; ++i) {
+            if (i >= k && nums[(i - k) % n] == 1) {
+                --ones;
+            }
+            if (nums[i % n] == 1) {
+                ++ones;
+            }
+            maxOnes = Math.max(maxOnes, ones);
+        }
+
+        return k - maxOnes;
+    }
+}
