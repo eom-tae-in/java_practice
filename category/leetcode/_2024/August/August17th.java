b85151861a3b802b3c8a0930befa94274c0c3fa2
eom-tae-in <eti0728@daum.net>
2024-08-17 21:53:43 +0900

    [feat] solve August 17th

---
+++ category/leetcode/_2024/August/August17th.java
@@ -0,0 +1,30 @@
+package category.leetcode._2024.August;
+
+import java.util.Arrays;
+
+public class August17th {
+
+    public long maxPoints(final int[][] points) {
+        final int n = points[0].length;
+        long[] dp = new long[n];
+        for (int[] row : points) {
+            long[] leftToRight = new long[n];
+            long runningMax = 0;
+            for (int j = 0; j < n; ++j) {
+                runningMax = Math.max(runningMax - 1, dp[j]);
+                leftToRight[j] = runningMax;
+            }
+            long[] rightToLeft = new long[n];
+            runningMax = 0;
+            for (int j = n - 1; j >= 0; --j) {
+                runningMax = Math.max(runningMax - 1, dp[j]);
+                rightToLeft[j] = runningMax;
+            }
+            for (int j = 0; j < n; ++j) {
+                dp[j] = Math.max(leftToRight[j], rightToLeft[j]) + row[j];
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsLong();
+    }
+}
