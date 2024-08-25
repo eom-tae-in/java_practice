8d1fc2f644d5f9e918c0f203fbe4c88e3e2b7e7a
eom-tae-in <eti0728@daum.net>
2024-08-25 17:00:58 +0900

    [feat] solve August 25th

---
+++ category/leetcode/_2024/August/August25th.java
@@ -0,0 +1,43 @@
+package category.leetcode._2024.August;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class August25th {
+
+    public List<Integer> postorderTraversal(final TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        postorder(root, ans);
+
+        return ans;
+    }
+
+    private void postorder(final TreeNode root, final List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, ans);
+        postorder(root.right, ans);
+        ans.add(root.val);
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(final int val) {
+        this.val = val;
+    }
+
+    TreeNode(final int val, final TreeNode left, final TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
