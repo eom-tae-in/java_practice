4c32da383681335bf52212a6382411e9101d93b5
eom-tae-in <eti0728@daum.net>
2024-10-08 17:47:10 +0900

    [feat] solve october 8th

---
+++ category/leetcode/_2024/october/October8th.java
@@ -0,0 +1,19 @@
+package category.leetcode._2024.october;
+
+public class October8th {
+
+    public int minSwaps(final String s) {
+        int unmatched = 0;
+
+        for (final char c : s.toCharArray()) {
+            if (c == '[') {
+                unmatched++;
+            } else if (unmatched > 0) {
+                unmatched--;
+            }
+        }
+
+        return (unmatched + 1) / 2;
+    }
+}
+`
