2766649435da2a5b0fafb713a23f12b292d076ef
eom-tae-in <eti0728@daum.net>
2024-10-17 11:01:30 +0900

    [feat] solve october 17th

---
+++ category/leetcode/_2024/october/October17th.java
@@ -0,0 +1,25 @@
+package category.leetcode._2024.october;
+
+public class October17th {
+
+    public int maximumSwap(int num) {
+        char[] s = Integer.toString(num).toCharArray();
+        int[] lastIndex = new int[10];
+
+        for (int i = 0; i < s.length; ++i) {
+            lastIndex[s[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < s.length; ++i) {
+            for (int d = 9; d > s[i] - '0'; --d) {
+                if (lastIndex[d] > i) {
+                    s[lastIndex[d]] = s[i];
+                    s[i] = (char) ('0' + d);
+                    return Integer.parseInt(new String(s));
+                }
+            }
+        }
+
+        return num;
+    }
+}
