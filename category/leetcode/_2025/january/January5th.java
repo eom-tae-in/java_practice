fb861781f0fda11b013f55a08d3d5ebf91863c73
eom-tae-in <eti0728@daum.net>
2025-01-06 13:04:06 +0900

    [feat] solve january 5th

--- category/leetcode/_2025/january/January5th.java
+++ category/leetcode/_2025/january/January5th.java
@@ -1,2 +1,27 @@
-package category.leetcode._2025.january;public class January5yh {
+package category.leetcode._2025.january;
+
+public class January5th {
+
+    public String shiftingLetters(final String s, final int[][] shifts) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int currShift = 0;
+        int[] timeline = new int[s.length() + 1];
+
+        for (int[] shift : shifts) {
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2];
+            int diff = direction == 1 ? 1 : -1;
+            timeline[start] += diff;
+            timeline[end + 1] -= diff;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            currShift = (currShift + timeline[i]) % 26;
+            int num = (s.charAt(i) - 'a' + currShift + 26) % 26;
+            stringBuilder.append((char) ('a' + num));
+        }
+
+        return stringBuilder.toString();
+    }
 }
