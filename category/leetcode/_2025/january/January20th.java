9cf3ba6ce6a6a013786eea3a56627c90d76fa167
eom-tae-in <eti0728@daum.net>
2025-01-21 09:57:30 +0900

    [feat] add code (january 20th)

--- category/leetcode/_2025/january/January20th.java
+++ category/leetcode/_2025/january/January20th.java
@@ -1,2 +1,38 @@
-package category.leetcode._2025.january;public class January20th {
+package category.leetcode._2025.january;
+
+public class January20th {
+
+    public int firstCompleteIndex(final int[] arr, final int[][] mat) {
+        int rowLength = mat.length;
+        int columnLength = mat[0].length;
+        int[] rows = new int[rowLength];
+        int[] columns = new int[columnLength];
+        int[] rowIndices = new int[rowLength * columnLength + 1];
+        int[] columnIndices = new int[rowLength * columnLength + 1];
+
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                rowIndices[mat[i][j]] = i;
+                columnIndices[mat[i][j]] = j;
+            }
+        }
+
+        int arrLength = arr.length;
+
+        for (int i = 0; i < arrLength; i++) {
+            int current = arr[i];
+            int row = rowIndices[current];
+            int column = columnIndices[current];
+
+            if (++rows[row] == columnLength) {
+                return i;
+            }
+
+            if (++columns[column] == rowLength) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
 }
