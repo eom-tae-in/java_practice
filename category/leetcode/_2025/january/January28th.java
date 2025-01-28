26863be34822aa19701a834403ed801d836ece24
eom-tae-in <eti0728@daum.net>
2025-01-28 10:14:40 +0900

    fix january 28th

--- category/leetcode/_2025/january/January28th.java
+++ category/leetcode/_2025/january/January28th.java
@@ -3,33 +3,32 @@ package category.leetcode._2025.january;
 public class January28th {
 
     public int findMaxFish(final int[][] grid) {
-        int answer = 0;
+        int maxFish = 0;
 
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[0].length; ++j) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] > 0) {
-                    answer = Math.max(answer, dfs(grid, i, j));
+                    maxFish = Math.max(maxFish, dfs(grid, i, j));
                 }
             }
         }
 
-        return answer;
+        return maxFish;
     }
 
-    private int dfs(final int[][] grid, final int i, final int j) {
-        if (i < 0 || i == grid.length || j < 0 || j == grid.length) {
+    private int dfs(final int[][] grid, final int x, final int y) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) {
             return 0;
         }
 
-        if (grid[i][j] == 0) {
-            return 0;
-        }
+        int fishCount = grid[x][y];
+        grid[x][y] = 0;
 
-        int caughtFish = grid[i][j];
-        grid[i][j] = 0;
+        fishCount += dfs(grid, x + 1, y);
+        fishCount += dfs(grid, x - 1, y);
+        fishCount += dfs(grid, x, y + 1);
+        fishCount += dfs(grid, x, y - 1);
 
-        return caughtFish +
-                dfs(grid, i + 1, j) + dfs(grid, i - 1, j) +
-                dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
+        return fishCount;
     }
 }
