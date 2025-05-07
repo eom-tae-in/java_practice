d549b62de39301ce96a8d8dfde15efefdff54adc
eom-tae-in <eti0728@daum.net>
2025-05-07 13:17:34 +0900

    solve may 7th

---
+++ category/leetcode/_2025/may/May7th.java
@@ -0,0 +1,50 @@
+package category.leetcode._2025.may;
+
+import java.util.PriorityQueue;
+
+public class May7th {
+
+    private static final int[] DIR_X = {0, 1, 0, -1};
+    private static final int[] DIR_Y = {1, 0, -1, 0};
+
+    public int minTimeToReach(final int[][] moveTime) {
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+        int[][] minTime = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                minTime[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        pq.offer(new int[]{0, 0, 0});
+        minTime[0][0] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int x = curr[0], y = curr[1], time = curr[2];
+
+            if (x == n - 1 && y == m - 1) {
+                return time;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + DIR_X[d];
+                int ny = y + DIR_Y[d];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    int arrivalTime = Math.max(time, moveTime[nx][ny]) + 1;
+
+                    if (arrivalTime < minTime[nx][ny]) {
+                        minTime[nx][ny] = arrivalTime;
+                        pq.offer(new int[]{nx, ny, arrivalTime});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
