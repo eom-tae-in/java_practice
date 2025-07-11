daae7d0c7a9fbdc354e72bdd46677069861adf5a
eom-tae-in <eti0728@daum.net>
2025-07-11 16:26:53 +0900

    solve july 11th

---
+++ category/leetcode/_2025/july/July11th.java
@@ -0,0 +1,57 @@
+package category.leetcode._2025.july;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class July11th {
+
+    public int mostBooked(int n, int[][] meetings) {
+        int[] roomCount = new int[n];
+        Queue<Integer> usableRoom = new PriorityQueue<>();
+        Arrays.sort(meetings, Comparator.comparingInt((int[] row) -> row[0])
+                .thenComparingInt(row -> row[1]));
+
+        for (int i = 0; i < n; i++) {
+            usableRoom.offer(i);
+        }
+
+        Queue<Meeting> queue = new PriorityQueue<>(
+                Comparator.comparingLong((Meeting m) -> m.endTime)
+                        .thenComparingInt(m -> m.roomIndex)
+        );
+
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+
+            while (!queue.isEmpty() && queue.peek().endTime <= start) {
+                usableRoom.offer(queue.poll().roomIndex);
+            }
+
+            if (usableRoom.isEmpty()) {
+                Meeting poll = queue.poll();
+                roomCount[poll.roomIndex]++;
+                queue.offer(new Meeting(poll.roomIndex, poll.endTime + (end - start)));
+            } else {
+                int index = usableRoom.poll();
+                roomCount[index]++;
+                queue.offer(new Meeting(index, end));
+            }
+        }
+
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (roomCount[i] > roomCount[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private record Meeting(int roomIndex, long endTime) {
+    }
+}
