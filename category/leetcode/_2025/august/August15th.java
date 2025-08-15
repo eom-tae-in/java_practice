ddacb56d440f80512ab5efaf13f02445bbbbbfc9
eom-tae-in <eti0728@daum.net>
2025-08-15 09:10:21 +0900

    solve august 15th

---
+++ category/leetcode/_2025/august/August15th.java
@@ -0,0 +1,16 @@
+package category.leetcode._2025.august;
+
+public class August15th {
+
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
+}
