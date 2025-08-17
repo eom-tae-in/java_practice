1ef65d9d66e9be301036de14b0330abf7f5bb7d6
eom-tae-in <eti0728@daum.net>
2025-08-17 17:38:35 +0900

    solve august 17th

---
+++ category/leetcode/_2025/august/August17th.java
@@ -0,0 +1,29 @@
+package category.leetcode._2025.august;
+
+public class August17th {
+
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k - 1 + maxPts) {
+            return 1.0;
+        }
+
+        double answer = 0.0;
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+        double windowSum = dp[0];
+
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = windowSum / maxPts;
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                answer += dp[i];
+            }
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+
+        return answer;
+    }
+}
