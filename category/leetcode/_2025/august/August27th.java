7f216d8e6708698a1db2e7aed991f0f0a7055a85
eom-tae-in <eti0728@daum.net>
2025-08-27 17:51:36 +0900

    solve august 27th

---
+++ category/leetcode/_2025/august/August27th.java
@@ -0,0 +1,60 @@
+package category.leetcode._2025.august;
+
+public class August27th {
+
+    private static final int[][] DIRS = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+
+    public int lenOfVDiagonal(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][][] mem = new int[m][n][1 << 3];
+        int answer = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 1) {
+                    continue;
+                }
+
+                int[] maxs = {m - i, j + 1, i + 1, n - j};
+
+                for (int k = 0; k < 4; k++) {
+                    if (maxs[k] > answer) {
+                        answer = Math.max(answer, dfs(i, j, k, 1, 2, grid, mem) + 1);
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private int dfs(int i, int j, int k, int canTurn, int target, int[][] grid, int[][][] mem) {
+        i += DIRS[k][0];
+        j += DIRS[k][1];
+
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] != target) {
+            return 0;
+        }
+
+        int mask = k << 1 | canTurn;
+
+        if (mem[i][j][mask] > 0) {
+            return mem[i][j][mask];
+        }
+
+        int res = dfs(i, j, k, canTurn, 2 - target, grid, mem);
+
+        if (canTurn == 1) {
+            int[] maxs = {grid.length - i - 1, j, i, grid[i].length - j - 1};
+            k = (k + 1) % 4;
+
+            if (maxs[k] > res) {
+                res = Math.max(res, dfs(i, j, k, 0, 2 - target, grid, mem));
+            }
+        }
+
+        return mem[i][j][mask] = 1 + res;
+    }
+}
