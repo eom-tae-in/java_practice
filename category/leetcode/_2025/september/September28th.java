2aa97a3ccb140f0c9666c5106ce1a583bceb9d58
eom-tae-in <eti0728@daum.net>
2025-09-28 16:09:59 +0900

    solve september 28th

---
+++ category/leetcode/_2025/september/September28th.java
@@ -0,0 +1,18 @@
+package category.leetcode._2025.september;
+
+import java.util.Arrays;
+
+public class September28th {
+
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = nums.length - 3; i >= 0; i--) {
+            if (nums[i] + nums[i + 1] > nums[i + 2]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return 0;
+    }
+}
