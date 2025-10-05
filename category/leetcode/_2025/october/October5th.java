d6984e72e64f97966ccac6fa81fc11361fa1a89c
eom-tae-in <eti0728@daum.net>
2025-10-05 21:16:23 +0900

    solve october 5th

---
+++ category/leetcode/_2025/october/October5th.java
@@ -0,0 +1,51 @@
+package category.leetcode._2025.october;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class October5th {
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        List<List<Integer>> answer = new ArrayList<>();
+        boolean[][] seenP = new boolean[m][n];
+        boolean[][] seenA = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, i, 0, 0, seenP);
+            dfs(heights, i, n - 1, 0, seenA);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, 0, j, 0, seenP);
+            dfs(heights, m - 1, j, 0, seenA);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (seenP[i][j] && seenA[i][j]) {
+                    answer.add(new ArrayList<>(List.of(i, j)));
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(int[][] heights, int i, int j, int h, boolean[][] seen) {
+        if (i < 0 || i == heights.length || j < 0 || j == heights[0].length) {
+            return;
+        }
+
+        if (seen[i][j] || heights[i][j] < h) {
+            return;
+        }
+
+        seen[i][j] = true;
+        dfs(heights, i + 1, j, heights[i][j], seen);
+        dfs(heights, i - 1, j, heights[i][j], seen);
+        dfs(heights, i, j + 1, heights[i][j], seen);
+        dfs(heights, i, j - 1, heights[i][j], seen);
+    }
+}
