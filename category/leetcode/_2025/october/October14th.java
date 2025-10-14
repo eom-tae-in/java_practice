57ca8a66be9f2a93ef3b9d16d1245779ec188920
eom-tae-in <eti0728@daum.net>
2025-10-14 18:03:16 +0900

    solve october 14th

---
+++ category/leetcode/_2025/october/October14th.java
@@ -0,0 +1,26 @@
+package category.leetcode._2025.october;
+
+import java.util.List;
+
+public class October14th {
+
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int increasing = 1;
+        int prevIncreasing = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                increasing++;
+            } else {
+                prevIncreasing = increasing;
+                increasing = 1;
+            }
+
+            if (increasing / 2 >= k || Math.min(prevIncreasing, increasing) >= k) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
