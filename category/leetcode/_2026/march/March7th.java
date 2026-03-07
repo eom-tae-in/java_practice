343e467f047b834538e0525404d4d08cda434416
eom-tae-in <eti0728@daum.net>
2026-03-07 10:27:59 +0900

    solve march 7th

---
+++ category/leetcode/_2026/march/March7th.java
@@ -0,0 +1,23 @@
+package category.leetcode._2026.march;
+
+public class March7th {
+
+    public int minFlips(String s) {
+        int n = s.length();
+        int[][] count = new int[2][2];
+
+        for (int i = 0; i < n; i++) {
+            count[s.charAt(i) - '0'][i % 2]++;
+        }
+
+        int answer = Math.min(count[1][0] + count[0][1], count[0][0] + count[1][1]);
+
+        for (int i = 0; i < n; i++) {
+            count[s.charAt(i) - '0'][i % 2]--;
+            count[s.charAt(i) - '0'][(n + i) % 2]++;
+            answer = Math.min(answer, Math.min(count[1][0] + count[0][1], count[0][0] + count[1][1]));
+        }
+
+        return answer;
+    }
+}
