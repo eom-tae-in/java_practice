16eabfd48f66a512ff3e2ed1d16bdff8932d907d
eom-tae-in <eti0728@daum.net>
2026-03-09 12:27:02 +0900

    solve march 9th

---
+++ category/leetcode/_2026/march/March9th.java
@@ -0,0 +1,36 @@
+package category.leetcode._2026.march;
+
+public class March9th {
+
+    private static final int MOD = 1_000_000_007;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        long[][][] dp = new long[zero + 1][one + 1][2];
+
+        for (int i = 0; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+
+        for (int j = 0; j <= Math.min(one, limit); j++) {
+            dp[0][j][1] = 1;
+        }
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+
+                if (i - limit - 1 >= 0) {
+                    dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+                }
+
+                dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+
+                if (j - limit - 1 >= 0) {
+                    dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                }
+            }
+        }
+
+        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+    }
+}
