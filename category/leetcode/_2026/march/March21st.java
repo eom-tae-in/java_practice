b50d78359f341e97e37014a732e7f898fad92091
eom-tae-in <eti990728@naver.com>
2026-03-22 12:00:33 +0900

    refactor march 21st

--- category/leetcode/_2026/march/March21st.java
+++ category/leetcode/_2026/march/March21st.java
@@ -1,14 +1,17 @@
 package category.leetcode._2026.march;
 
-public class March10th {
+public class March21st {
 
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        int sc = y, ec = y + k - 1, sr = x;
+        int sc = y;
+        int ec = y + k - 1;
+        int sr = x;
+
         for (int j = sc; j <= ec; j++) {
             for (int i = 0; i < k / 2; i++) {
-                int tmp = grid[sr+i][j];
-                grid[sr+i][j] = grid[sr+k-i-1][j];
-                grid[sr+k-i-1][j] = tmp;
+                int tmp = grid[sr + i][j];
+                grid[sr + i][j] = grid[sr + k - i - 1][j];
+                grid[sr + k - i - 1][j] = tmp;
             }
         }
         return grid;
