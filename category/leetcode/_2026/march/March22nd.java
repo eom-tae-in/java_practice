9268000c9ba4ded90aac5456a45233fe9dd4cca8
eom-tae-in <eti990728@naver.com>
2026-03-22 12:45:25 +0900

    solve march 22nd

---
+++ category/leetcode/_2026/march/March22nd.java
@@ -0,0 +1,34 @@
+package category.leetcode._2026.march;
+
+import java.util.Arrays;
+
+public class March22nd {
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if (Arrays.deepEquals(mat, target)) {
+                return true;
+            }
+
+            rotate(mat);
+        }
+
+        return false;
+    }
+
+    private void rotate(int[][] mat) {
+        for (int i = 0, j = mat.length - 1; i < j; i++, j--) {
+            int[] temp = mat[i];
+            mat[i] = mat[j];
+            mat[j] = temp;
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = i + 1; j < mat.length; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
+}
