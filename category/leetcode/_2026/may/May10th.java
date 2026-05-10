e18ac11caf17c204c9fb4aff618cd369bd868a94
eom-tae-in <eti990728@naver.com>
2026-05-10 12:00:00 +0900

    solve may 10th

---
+++ category/leetcode/_2026/may/May10th.java
@@ -0,0 +1,23 @@
+package category.leetcode._2026.may;
+
+import java.util.Arrays;
+
+public class May10th {
+
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                if (dp[i] != -1 && Math.abs(nums[j] - nums[i]) <= target) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
