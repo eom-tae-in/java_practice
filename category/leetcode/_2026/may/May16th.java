6561b9efd823f47916b724c18e4f6946ae1bf861
eom-tae-in <eti990728@naver.com>
2026-05-16 22:03:43 +0900

    solve may 16th

---
+++ category/leetcode/_2026/may/May16th.java
@@ -0,0 +1,23 @@
+package category.leetcode._2026.may;
+
+public class May16th {
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+
+        return nums[left];
+    }
+}
