c5662cc3c05341e93af8ae7462873a6b32bbf354
eom-tae-in <eti990728@naver.com>
2026-08-16 19:26:21 +0900

    solve august 16th

---
+++ category/leetcode/_2026/august/August16th.java
@@ -0,0 +1,18 @@
+package category.leetcode._2026.august;
+
+public class August16th {
+
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
