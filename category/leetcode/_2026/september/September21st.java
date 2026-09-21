4ba872d9918afa0e79314b0380e7b720d704c59e
eom-tae-in <eti990728@naver.com>
2026-09-21 23:50:51 +0900

    solve september 21st

---
+++ category/leetcode/_2026/september/September21st.java
@@ -0,0 +1,27 @@
+package category.leetcode._2026.september;
+
+public class September21st {
+
+    public long[] resultArray(int[] A, int k) {
+        long[] res = new long[k];
+        int[] freq = new int[k];
+
+        for (int n : A) {
+            n %= k;
+            int[] cur = new int[k];
+            cur[n] = 1;
+
+            for (int x = 0; x < k; x++) {
+                cur[x * n % k] += freq[x];
+            }
+
+            freq = cur;
+
+            for (int x = 0; x < k; x++) {
+                res[x] += freq[x];
+            }
+        }
+
+        return res;
+    }
+}
